48f06ac6edc2720fac0f705eb6ef6b3f8faf014e
Sunsik <siksicking@gmail.com>
2020-03-25 14:50:53 +0900

    Added visualization results for simulation and real data analysis

--- StochasticFrontier/exmixture_bsa study.R
+++ StochasticFrontier/exmixture_bsa study.R
@@ -2,12 +2,14 @@ source("../StochasticFrontier/ex.R")
 source("../misc/make_Z.R")
 library(matrixStats)
 library(truncnorm)
+library(ggplot2)
+library(gridExtra)
 
 # Simulation study --------------------------------------------------------
 
 # Simulation size setting
-set.seed(1); R = 10 # R : Truncation level
-N = 80; T = 12; D = 8
+set.seed(5); R = 10 # R : Truncation level
+N = 50; T = 4; D = 8
 csrows = rep(T, N)
 csrows[round(N/4)] = T-2
 Z = make_Z(rep(T, N))
@@ -26,150 +28,90 @@ u = u[sample(1:N,N)] # Randomly shuffle the mixed inefficiencies
 rm("u1","u2")
 y = cbind(1, w)%*%beta - Z%*%u + f(x) + rnorm(nrow(Z))
 
-# VB
+# Estimation
 start = as.numeric(Sys.time())
-result = exmixture_bsa(y,x,w,Z,23,R,eps=1e-4) # truncate at R=10
+result = exmixture_bsa(y,x,w,Z,5,R) # truncate at R=10
 print(paste(round(as.numeric(Sys.time())-start, 4), "seconds elapsed."))
 
-# VB results
-# Manually exported as 800 * 320 in png type
-par(mar=c(4,3,2,1), mfrow=c(1,2))
-# VB: Fixed effect
+# Result visualization
+# Fixed effect(black point: true value, golden line: corresponding 95% credible interval)
 beta_ord = order(beta[-1])
-upper = qnorm(0.975,mean=result$mubeta.q[-1], sd=sqrt(diag(result$sigbeta.q)[-1]))
-lower = qnorm(0.025,mean=result$mubeta.q[-1], sd=sqrt(diag(result$sigbeta.q)[-1]))
-plot(1:D, beta[-1][beta_ord],
-     xlab="index",ylab="",
-     ylim=c(min(c(lower, beta)),max(c(upper, beta))),
-     pch=19, col=2,
-     main="Fixed effect(VB)")
-for (idx in 1:N) lines(c(idx,idx), c(upper[beta_ord][idx],lower[beta_ord][idx]))
+ggdata = data.frame(
+        upper = qnorm(0.975,mean=result$mubeta.q[-1], sd=sqrt(diag(result$sigbeta.q)[-1]))[beta_ord],
+        lower = qnorm(0.025,mean=result$mubeta.q[-1], sd=sqrt(diag(result$sigbeta.q)[-1]))[beta_ord],
+        beta = beta[-1][beta_ord],
+        index = 1:D)
+fe = ggplot() +
+        geom_errorbar(data=ggdata, aes(x=index, ymin=lower, ymax=upper), size=1.1, color="darkorange3", width=0.3) +
+        geom_point(data=ggdata, aes(x=index, y=beta), size=1.1) +
+        theme_bw() +
+        xlab("Arranged index") +
+        ylab("") +
+        ggtitle("Fixed effects")
 
-# VB: Random effect
+# Random effect(black point: true value, golden line: corresponding 95% credible interval)
 u_ord = order(u)
-upper = qtruncnorm(0.975,a=0,mean=result$muu.q, sd=sqrt(result$sigu.q))
-lower = qtruncnorm(0.025,a=0,mean=result$muu.q, sd=sqrt(result$sigu.q))
-plot(1:N, u[u_ord],xlab="idx",ylab="",
-     pch=19,col=2,
-     ylim=c(min(c(0,lower,u)),max(c(upper, u))),
-     main="Inefficiency(VB)")
-for (idx in 1:N) lines(c(idx,idx), c(upper[u_ord][idx],lower[u_ord][idx]))
+ggdata = data.frame(
+        upper = qnorm(0.975,mean=result$muu.q, sd=sqrt(result$sigu.q))[u_ord],
+        lower = qnorm(0.025,mean=result$muu.q, sd=sqrt(result$sigu.q))[u_ord],
+        u = u[u_ord],
+        index = 1:N)
+re = ggplot() +
+        geom_errorbar(data=ggdata, aes(x=index, ymin=lower, ymax=upper), size=1.1, color="darkorange3", width=0.3) +
+        geom_point(data=ggdata, aes(x=index, y=u), size=1.1) +
+        theme_bw() +
+        xlab("Arranged index") +
+        ylab("") +
+        ggtitle("Random effects")
 
-# VB: Nonparametric
-# Manually exported as 600 * 400 in png type
-par(mfrow=c(1,1))
-res = y-(cbind(1,w)%*%result$mubeta.q - Z%*%result$muu.q)
-plot(x,res, main="Fitted mean curve(VB)", ylab="")
-lines(x[ord],result$post_curve[ord],lwd=3,col=2)
-lines(x[ord],result$post_upper[ord],lwd=2,lty=2)
-lines(x[ord],result$post_lower[ord],lwd=2,lty=2)
+# Nonparametric(black point: residual, golden line: mean curve)
+vb_polygon = data.frame(
+        variate = c(x[ord], rev(x[ord])),
+        area = c(result$post_upper[ord], rev(result$post_lower[ord])))
+residual = data.frame(
+        variate = x,
+        residual = y-(cbind(1,w)%*%result$mubeta.q - Z%*%result$muu.q))
+mc = ggplot() +
+        geom_point(data=residual,
+                   mapping=aes(x=variate,y=residual),size=1.1) +
+        geom_polygon(data=vb_polygon,aes(x=variate,y=area), fill="darkorange3", alpha=0.3) +
+        geom_line(data=data.frame(x=x[ord], y=result$post_curve[ord]),
+                  mapping=aes(x=x,y=y),color="darkorange3",size=1.1) +
+        xlab("Nonparametric variate")+
+        ylab("Residuals")+
+        theme_bw() +
+        ggtitle("Mean curve")
 
-# MCMC
-csrows = cumsum(apply(Z,2,sum))
-csrows = c(0,csrows)
-ylist = xlist = wlist = list()
-for(i in 1:ncol(Z)){
-        ylist[[i]] = y[(csrows[i]+1):csrows[i+1]]
-        xlist[[i]] = x[(csrows[i]+1):csrows[i+1]]
-        wlist[[i]] = w[(csrows[i]+1):csrows[i+1],]}
-source("./MCMC/bsfr_main.R")
-setwd("./MCMC/")
-mcmc = list(nblow=1000,nskip=5,smcmc=2000,ndisp=5000)
-start = as.numeric(Sys.time())
-mcmcresult = bsfr.main(y=ylist,w=wlist,x=xlist,nbasis=5,random='dpm',mcmc=mcmc,shape='Free')
-print(paste("MCMC:", round(as.numeric(Sys.time()) - start, 3), "seconds elapsed"))
-beta0 = coef.bsfm(mcmcresult)$beta[1]
-mcmc_fit = fitted.bsfm(mcmcresult)
-res_MC = y-beta0-mcmc_fit$wbeta$mean-Z%*%mcmc_fit$bhat$mean
-
-# MCMC results
-# Manually exported as 800 * 320 in png type
-par(mar=c(4,3,2,1), mfrow=c(1,2))
-# MCMC: Fixed effect
-upper = apply(mcmc_fit$mcmc.draws$beta, 2, quantile, 0.975)[-1]
-lower = apply(mcmc_fit$mcmc.draws$beta, 2, quantile, 0.025)[-1]
-plot(1:D, beta[-1][beta_ord],
-     xlab="index",ylab="",
-     ylim=c(min(c(lower, beta)),max(c(upper, beta))),
-     pch=19, col=2,
-     main="Fixed effect(MCMC)")
-for (idx in 1:N) lines(c(idx,idx), c(upper[beta_ord][idx],lower[beta_ord][idx]))
-
-# MCMC: Random effect
-upper = rev(apply(mcmc_fit$mcmc.draws$b, 2, quantile, 0.975)[u_ord])
-lower = rev(apply(mcmc_fit$mcmc.draws$b, 2, quantile, 0.025)[u_ord])
-plot(1:N, u[u_ord],xlab="idx",ylab="",
-     pch=19,col=2,
-     ylim=c(min(c(0,lower,u)),max(c(upper, u))),
-     main="Inefficiency(MCMC)")
-for (idx in 1:N) lines(c(idx,idx), c(upper[idx],lower[idx]))
-
-# MCMC: Nonparametric
-# Manually exported as 600 * 400 in png type
-par(mfrow=c(1,1))
-plot(x,res_MC, main="Fitted mean curve(MCMC)", ylab="")
-lines(mcmc_fit$xgrid,mcmc_fit$fxgrid$mean,lwd=3,col=2)
-lines(mcmc_fit$xgrid,mcmc_fit$fxgrid$lower,lwd=2,lty=2)
-lines(mcmc_fit$xgrid,mcmc_fit$fxgrid$upper,lwd=2,lty=2)
-
+# Export
+png("./figures/simulation.png",width=900,height=320)
+grid.arrange(fe, re, mc, nrow=1)
+dev.off()
 
 # University --------------------------------------------------------------
-
 library(dplyr)
-data = read.csv("../univ.csv")
-data = data %>% select(univ_id,lneduc,lngrad,lnunder,lnw_ac,lnw_nac,
-                       d_nohos,d_sci,d_arts,d_med,d_educ,lnkaken,p_sub)%>% 
+data = read.csv("./univ.csv")
+data = data %>% 
         mutate(id=factor(data$univ_id,labels=1:80)) %>% 
-        mutate(year=c(rep(1:12,35),1:10,rep(1:12,44))) # 36th obs has 10 years data, others have 12 years
-head(data)
-
+        filter(year >= 7) %>% 
+        select(univ_id,lneduc,lngrad,lnunder,lnw_ac,lnw_nac,
+                       d_nohos,d_sci,d_arts,d_med,d_educ)
 nobs  = nrow(data)
 id    = as.numeric(data$id)
 nid   = length(unique(data$univ_id))
 y = data$lneduc
 x = data$lnunder; ord = order(x)
 w = data %>% select(lngrad, lnw_ac, lnw_nac, d_nohos, d_sci, d_arts, d_med, d_educ) %>% as.matrix()
-head(w)
-Z = data %>% 
-        group_by(id) %>% 
-        summarize(count=n()) %>% 
-        select(count) %>% 
-        unlist() %>% 
-        make_Z()
+Z = data %>% group_by(univ_id) %>% summarize(count=n()) %>% select(count) %>% unlist() %>% make_Z()
 
-# University: VB ----------------------------------------------------------
-
+# VB Estimation
 start = as.numeric(Sys.time())
-result = ex_bsa(y,x,w,Z,J=2,productivity=FALSE)
+vbresult_dpm = exmixture_bsa(y,x,w,Z,J=10,productivity=FALSE)
 print(paste(round(as.numeric(Sys.time())-start, 4), "seconds elapsed."))
 
-# VB: Fixed effect
-mean_fixed = result$mubeta.q[-1]; beta_ord=order(mean_fixed)
-upper = qnorm(0.975,mean=result$mubeta.q[-1], sd=sqrt(diag(result$sigbeta.q)[-1]))[beta_ord]
-lower = qnorm(0.025,mean=result$mubeta.q[-1], sd=sqrt(diag(result$sigbeta.q)[-1]))[beta_ord]
-plot(1:ncol(w), mean_fixed[beta_ord],
-     xlab="index",ylab="",
-     ylim=c(min(c(lower, mean_fixed)),max(c(upper, mean_fixed))),
-     pch=19, col=2,
-     main="Fixed effect(VB)")
-for (idx in 1:ncol(w)) lines(c(idx,idx), c(upper[idx],lower[idx]))
-
-# Random effect
-mean_random = result$muu.q; u_ord=order(mean_random)
-upper = qtruncnorm(0.975,a=0,mean=result$muu.q, sd=sqrt(result$sigu.q))[u_ord]
-lower = qtruncnorm(0.025,a=0,mean=result$muu.q, sd=sqrt(result$sigu.q))[u_ord]
-plot(1:ncol(Z),mean_random[u_ord],xlab="idx",ylab="",
-     pch=19,col=2,
-     ylim=c(min(c(0,lower,mean_random)),max(c(upper, mean_random))),
-     main="Inefficiency(VB)")
-for (idx in 1:ncol(Z)) lines(c(idx,idx), c(upper[idx],lower[idx]))
-        
-# Nonparametric
-res = y-(cbind(1,w)%*%result$mubeta.q + Z%*%result$muu.q)
-plot(x,res, main="Fitted mean curve(VB)", ylab="")
-lines(x[ord],result$post_curve[ord],lwd=3,col=2)
-
-# University: MCMC --------------------------------------------------------
+# MCMC estimation
+source("./MCMC/bsfr_main.R")
+setwd("./MCMC/")
+set.seed(10)
 csrows = cumsum(apply(Z,2,sum))
 csrows = c(0,csrows)
 ylist = xlist = wlist = list()
@@ -177,44 +119,82 @@ for(i in 1:ncol(Z)){
         ylist[[i]] = y[(csrows[i]+1):csrows[i+1]]
         xlist[[i]] = x[(csrows[i]+1):csrows[i+1]]
         wlist[[i]] = w[(csrows[i]+1):csrows[i+1],]}
+# MCMC parameters
 mcmc = list(nblow=10000,nskip=5,smcmc=20000,ndisp=5000)
 start = as.numeric(Sys.time())
-mcmcresult = bsfr.main(y=ylist,w=wlist,x=xlist,nbasis=2,random='exp',mcmc=mcmc,shape='Free')
+foutr_dpm = bsfr.main(y=ylist,w=wlist,x=xlist,nbasis=10,random='dpm',mcmc=mcmc,shape='Free')
 print(paste("MCMC:", round(as.numeric(Sys.time()) - start, 3), "seconds elapsed"))
-beta0 = coef.bsfm(mcmcresult)$beta[1]
-mcmc_fit = fitted.bsfm(mcmcresult)
-res_MC = y-beta0-mcmc_fit$wbeta$mean-Z%*%mcmc_fit$bhat$mean
+beta0 = coef.bsfm(foutr_dpm)$beta[1]
+ffitr_dpm = fitted.bsfm(foutr_dpm)
+res_MC = y-beta0-ffitr_dpm$wbeta$mean-Z%*%ffitr_dpm$bhat$mean
 
-# MCMC results
-# Manually exported as 800 * 320 in png type
-par(mar=c(4,3,2,1), mfrow=c(1,2))
-# MCMC: Fixed effect
-mean_fixed = apply(mcmc_fit$mcmc.draws$beta,2,mean)[-1]; beta_ord=order(mean_fixed)
-upper = apply(mcmc_fit$mcmc.draws$beta, 2, quantile, 0.975)[-1][beta_ord]
-lower = apply(mcmc_fit$mcmc.draws$beta, 2, quantile, 0.025)[-1][beta_ord]
-plot(1:ncol(w), mean_fixed[beta_ord],
-     xlab="index",ylab="",
-     ylim=c(min(c(lower, mean_fixed)),max(c(upper, mean_fixed))),
-     pch=19, col=2,
-     main="Fixed effect(MCMC)")
-for (idx in 1:ncol(w)) lines(c(idx,idx), c(upper[idx],lower[idx]))
+# Values to compare through visualization
+vb_fixed = vbresult_dpm$mubeta.q[-1]
+mc_fixed = apply(ffitr_dpm$mcmc.draws$beta,2,mean)[-1]
+means = c(vb_fixed, mc_fixed)
+upper = c(qnorm(0.025, vb_fixed, sqrt(diag(vbresult_dpm$sigbeta.q)[-1])), apply(ffitr_dpm$mcmc.draws$beta, 2, quantile, 0.025)[-1])
+lower = c(qnorm(0.975, vb_fixed, sqrt(diag(vbresult_dpm$sigbeta.q)[-1])), apply(ffitr_dpm$mcmc.draws$beta, 2, quantile, 0.975)[-1])
+fixed = data.frame(
+        names = factor(dimnames(w)[[2]],levels=rev(dimnames(w)[[2]])),
+        mean = means, upper = upper, lower = lower,
+        method = factor(rep(c("VB","MCMC"), each=length(means)/2), levels=c("MCMC","VB")))
+random = data.frame(
+        MCMC = apply(ffitr_dpm$mcmc.draws$b, 2, mean), 
+        VB = vbresult_dpm$muu.q)
 
-# MCMC: Random effect
-mean_random = apply(mcmc_fit$mcmc.draws$b,2,mean); u_ord=order(mean_random)
-upper = apply(mcmc_fit$mcmc.draws$b, 2, quantile, 0.975)[u_ord]
-lower = apply(mcmc_fit$mcmc.draws$b, 2, quantile, 0.025)[u_ord]
-plot(1:ncol(Z),mean_random[u_ord],xlab="idx",ylab="",
-     pch=19,col=2,
-     ylim=c(min(c(0,lower,mean_random)),max(c(upper, mean_random))),
-     main="Inefficiency(MCMC)")
-for (idx in 1:ncol(Z)) lines(c(idx,idx), c(upper[idx],lower[idx]))
+# Fixed effects
+fe = ggplot(fixed, aes(x=names, y=mean, color=method)) + 
+        geom_errorbar(data=fixed, mapping=aes(ymin=lower, ymax=upper), position="dodge") +
+        geom_point(position=position_dodge(width=0.9), size=1.6) +
+        geom_hline(yintercept = 0, color="black", size=0.7, linetype="dotted") +
+        scale_color_manual(values=c("deepskyblue4","darkorange3")) +
+        xlab("") + ylab("") +
+        coord_flip() +
+        theme_bw() +
+        theme(panel.grid.major.y = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.text = element_text(size=11),
+              panel.grid = element_blank(),
+              legend.position = c(0.17,0.83)) +
+        ggtitle("Fixed effects")
 
-# MCMC: Nonparametric
-# Manually exported as 600 * 400 in png type
-par(mfrow=c(1,1))
-upper=mcmc_fit$fxgrid$upper
-lower=mcmc_fit$fxgrid$lower
-plot(x,res_MC,ylim=c(min(res_MC, lower), max(res_MC,upper)),main="Fitted mean curve(MCMC)", ylab="")
-lines(mcmc_fit$xgrid,mcmc_fit$fxgrid$mean,lwd=3,col=2)
-lines(mcmc_fit$xgrid,lower,lwd=2,lty=2)
-lines(mcmc_fit$xgrid,upper,lwd=2,lty=2)
+# Random effects
+re = ggplot(random, aes(x=MCMC, y=VB)) +
+        geom_point() +
+        geom_abline(slope=1, intercept=0) +
+        theme_classic() +
+        ggtitle("Random effects")
+
+# Nonlinear curve
+mcmc_polygon = data.frame(
+        variate = c(ffitr_dpm$xgrid, rev(ffitr_dpm$xgrid)),
+        area = c(ffitr_dpm$fxgrid$upper, rev(ffitr_dpm$fxgrid$lower)))
+vb_polygon = data.frame(
+        variate = c(x[ord], rev(x[ord])),
+        area = c(vbresult_dpm$post_upper[ord], rev(vbresult_dpm$post_lower[ord])))
+residual = data.frame(
+        variate=rep(x,2),
+        residual=c(unlist(y)-ffitr_dpm$wbeta$mean-ffitr_dpm$bhat$mean[id],
+                   y-cbind(1,w)%*%vbresult_dpm$mubeta.q-Z%*%vbresult_dpm$muu.q),
+        method=rep(c("MCMC","VB"),each=length(x)))
+mc = ggplot() +
+        geom_point(data=residual,
+                   mapping=aes(x=variate,y=residual,color=method,shape=method)) +
+        scale_color_manual(values=c("deepskyblue4","darkorange3"))+
+        geom_polygon(data=mcmc_polygon,aes(x=variate,y=area), fill="deepskyblue4", alpha=0.3) +
+        geom_polygon(data=vb_polygon,aes(x=variate,y=area), fill="darkorange3", alpha=0.3) +
+        geom_line(data=data.frame(x=ffitr_dpm$xgrid, y=ffitr_dpm$fxgrid$mean),
+                  mapping=aes(x=x,y=y),color="deepskyblue4",size=1.5) +
+        geom_line(data=data.frame(x=x[ord], y=vbresult_dpm$post_curve[ord]),
+                  mapping=aes(x=x,y=y),color="darkorange3",size=1.5) +
+        xlab("lnunder")+
+        ylab("Residuals")+
+        theme_bw()+
+        ggtitle("Mean curve") +
+        theme(legend.position = c(0.85,0.15),
+              panel.grid = element_blank())
+
+# Export
+png("../figures/university.png",width=900,height=320)
+grid.arrange(fe, re, mc, nrow=1)
+dev.off()
