6f1db253e3310e02d3359753d536fda348cf6121
Sunsik <siksicking@gmail.com>
2020-04-21 18:33:14 +0900

    Algorithm converges, but numerical underflow occurs

--- MeasurementError/mer.R
+++ MeasurementError/mer.R
@@ -1,25 +1,25 @@
 mer = function(y, w, v, prior=NULL, maxiter=500, tol=1e-4, n_grid=1e3, nknots=10)
 {
-  library(splines)
-  library(matrixStats)
   N = length(y)
   D = ncol(w)
-  W = cbind(1, w)
+  W = cbind(1,w)
   WtW = crossprod(W)
   
-  # Define hyperparameters
+  # Hyperparameters
   if (is.null(prior))
   {
     mubeta.0 = rep(0, D+1)
     sigbeta.0 = diag(rep(100, D+1))
-    sigv2 = 1
-    sigmu2 = 100
+    sig2v = 1
+    sig2mu = 100
     axi = bxi = atau = btau = asig = bsig = 1e-3
-  } else {
+  }
+  else
+  {
     mubeta.0 = prior$mubeta.0
     sigbeta.0 = prior$sigbeta.0
-    sigv2 = prior$sigv2
-    sigmu2 = prior$sigmu2
+    sig2v = prior$sig2v
+    sig2mu = prior$sig2mu
     axi = prior$axi
     bxi = prior$bxi
     atau = prior$atau
@@ -31,26 +31,25 @@ mer = function(y, w, v, prior=NULL, maxiter=500, tol=1e-4, n_grid=1e3, nknots=10
   sbimb0 = sb0i %*% mubeta.0
   
   # Define grids
-  err = diff(range(v))
-  grids = seq(min(v)-err/10, max(v)+err/10, length.out=n_grid)
-  knots = quantile(grids, seq(0,1,length.out=nknots)[-c(1,nknots)])
-  vphi_g = bs(grids, knots=knots, intercept=FALSE) # M*J
+  deviance = diff(range(v))
+  grid = seq(min(v)-deviance/10, max(v)+deviance/10, length.out=n_grid)
+  
+  # Construct spline bases on these grids
+  knots = quantile(grid, seq(0,1,length=(nknots+2))[-c(1,nknots+2)])
+  vphi_g = splines::bs(grid, knots=knots, degree=3, intercept=TRUE) # M*J
   J = ncol(vphi_g)
   
-  # Initialize hyperparameters
-  knots = quantile(v, seq(0,1,length.out=nknots)[-c(1,nknots)])
-  vphiq = bs(v, knots=knots, intercept=FALSE)
+  # Initialize variational parameters
+  sig.ratio = tau.ratio = xi.ratio = 1
   mubeta.q = rep(0, D+1)
   mutheta.q = rep(0, J)
-  sigtheta.q = diag(rep(1, J))
-  sig.ratio = asig/bsig
-  tau.ratio = atau/btau
-  xi.ratio = axi/bxi
-  axitl = axi+(N/2)
-  atautl = atau+(J/2)
-  asigtl = asig+(N/2)
+  sigtheta.q = diag(rep(10, J))
+  asigtl = asig + (N/2)
+  atautl = atau + (J/2)
+  axitl = axi + (N/2)
   mutl = 0
-  qx = qx2 = qvarx = rep(0,N)
+  vphiq = matrix(0, N, J)
+  ex = ex2 = rep(0, N)
   
   # Update as
   for (iter in 1:maxiter)
@@ -60,66 +59,61 @@ mer = function(y, w, v, prior=NULL, maxiter=500, tol=1e-4, n_grid=1e3, nknots=10
     
     # beta
     sigbeta.q = solve(sb0i + sig.ratio*WtW)
-    mubeta.q = sigbeta.q%*%(sbimb0 + sig.ratio*t(W)%*%(y-vphiq%*%mutheta.q))
+    mubeta.q = sigbeta.q %*% (sbimb0 + sig.ratio*t(W)%*%(y-vphiq%*%mutheta.q))
     mubeta.q = drop(mubeta.q)
     
-    # denoised value(x)
-    common = sig.ratio*diag(vphi_g%*%(outer(mutheta.q,mutheta.q)+sigtheta.q)%*%t(vphi_g)) + (xi.ratio+1/sigv2)*(grids^2) - 2*xi.ratio*mutl*grids
+    # denoised variate(x)
+    common = -0.5*(sig.ratio*diag(vphi_g%*%(outer(mutheta.q, mutheta.q)+sigtheta.q)%*%t(vphi_g)) + (xi.ratio+(1/sig2v))*grid^2 - 2*mutl*xi.ratio*grid) # M*1
     resid = drop(y-W%*%mubeta.q)
     for (n in 1:N)
     {
-      pmgrid = -0.5*common + v[n]*grids/sigv2 + sig.ratio*resid[n]*drop(vphi_g%*%mutheta.q)
-      lognorm = logSumExp(pmgrid)
-      gstar = max(log(abs(grids))+pmgrid)
-      colstar = apply(abs(vphi_g)+pmgrid,2,max)
-      
-      qx[n] = exp(log(sum(sign(grids) * exp(log(abs(grids))+pmgrid-gstar))) + gstar - lognorm)
-      qx2[n] = exp(logSumExp(2*log(abs(grids))+pmgrid) - lognorm)
-      vphiq[n,] = exp(log(apply(sign(vphi_g) * exp(log(abs(vphi_g))+pmgrid-outer(rep(1,n_grid), colstar)),2,sum)) + colstar - lognorm)
+      pmgrid = common + v[n]*grid/sig2v + sig.ratio*resid[n]*vphi_g%*%mutheta.q # M*1
+      normalizer = sum(exp(pmgrid))
+      ex[n] = sum(grid*exp(pmgrid))/normalizer
+      ex2[n] = sum(grid^2*exp(pmgrid))/normalizer
+      vphiq[n,] = drop(t(vphi_g)%*%exp(pmgrid))/normalizer
     }
-    qvarx = qx2-(qx)^2
     vphitvphiq = t(vphiq)%*%vphiq
+    varx = ex2 - (ex)^2
     
     # theta
-    sigtheta.q = tau.ratio*diag(J) + sig.ratio*vphitvphiq
-    mutheta.q = sig.ratio*sigtheta.q%*%t(vphiq)%*%(y-W%*%mubeta.q)
+    sigtheta.q = solve(tau.ratio*diag(J) + sig.ratio*vphitvphiq)
+    mutheta.q = sig.ratio * sigtheta.q%*%t(vphiq)%*%(y-W%*%mubeta.q)
     mutheta.q = drop(mutheta.q)
     
     # mu
-    sigmu2tl = 1/(1/sigmu2+N*xi.ratio)
-    mutl = xi.ratio*sigmu2tl*sum(qx)
+    sig2mutl = 1/((1/sig2mu)+xi.ratio*N)
+    mutl = sig2mutl*xi.ratio*sum(ex)
     
     # xi
-    bxitl = bxi + 0.5*(sum((qx-mutl)^2) + sum(qvarx) + N*sigmu2tl)
-    xi.ratio = axitl / bxitl
+    bxitl = bxi + 0.5*(sum((ex-mutl)^2)+sum(varx)+N*sig2mutl)
+    xi.ratio = axitl/bxitl
     
     # tau
-    btautl = btau + 0.5*sum(mutheta.q^2+diag(sigtheta.q))
-    tau.ratio = atautl / btautl
+    btautl = btau + 0.5*(sum(mutheta.q^2)+sum(diag(sigtheta.q)))
+    tau.ratio = atautl/btautl
     
     # sigma
     ssterm = sum((y-W%*%mubeta.q-vphiq%*%mutheta.q)^2)
     trterm1 = sum(diag(WtW%*%sigbeta.q))
     trterm2 = sum(diag(vphitvphiq%*%sigtheta.q))
     bsigtl = bsig + 0.5*(ssterm+trterm1+trterm2)
-    sig.ratio = asigtl / bsigtl
+    sig.ratio = asigtl/bsigtl
     
     # Convergence
-    bool1 = mean((mubeta.q.old-mubeta.q)^2) < tol
-    bool2 = mean((mutheta.q.old-mutheta.q)^2) < tol
-    if (bool1&bool2) {
+    bool1 = sqrt(mean((mubeta.q-mubeta.q.old)^2)) < tol
+    bool2 = sqrt(mean((mutheta.q-mutheta.q.old)^2)) < tol
+    if (bool1&bool2) 
+    {
       break
-    } else {
-      print("")
-      print(mean((mubeta.q.old-mubeta.q)^2))
-      print(mean((mutheta.q.old-mutheta.q)^2))
-      print("")
+    }
+    else
+    {
+      cat("beta :", sqrt(mean((mubeta.q-mubeta.q.old)^2)), "\ttheta :", sqrt(mean((mutheta.q-mutheta.q.old)^2)), "\n")
+      if(is.nan(sqrt(mean((mutheta.q-mutheta.q.old)^2)))) browser()
     }
   }
-  print(iter)
-  return(list(
-    mubeta.q=mubeta.q, sigbeta.q=sigbeta.q, qx=qx
-  ))
+  print("##### CLEAR #####")
 }
 
 # y = wb + f(x)
