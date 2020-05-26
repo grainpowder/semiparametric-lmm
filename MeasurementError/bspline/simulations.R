23e95b2d6833f30531590ace5ad9f146e716cc45
Sunsik <siksicking@gmail.com>
2020-05-26 10:38:02 +0900

    Implemented semiparametric MER with B-spline

--- MeasurementError/bspline/simulations.R
+++ MeasurementError/bspline/simulations.R
@@ -1,10 +1,14 @@
+
+
+# Standard Non/Semiparametric ---------------------------------------------
+
 source("regression.R")
 set.seed(10)
 N = 100
 f = function(x) 2*x+sin(2*pi*x)
 x = runif(N) * 2
 
-# Nonparametric -----------------------------------------------------------
+# Nonparametric
 
 set.seed(10)
 y = 2 + f(x) + rnorm(N, sd=0.5)
@@ -17,7 +21,7 @@ lines(x[ord],vb_result$post_upper[ord],lwd=2,lty=2)
 lines(x[ord],vb_result$post_lower[ord],lwd=2,lty=2)
 plot(vb_result$lb, ylab="ELBO", xlab="Iteration", main="ELBO plot")
 
-# Semiparametric ----------------------------------------------------------
+# Semiparametric
 
 set.seed(10)
 D = 5
@@ -34,3 +38,31 @@ lines(x[ord],vb_result$post_lower[ord],lwd=2,lty=2)
 plot(beta[-1], vb_result$mubeta.q[-1], xlab="True", ylab="Estimated", main="Fixed Effects")
 lines(-10:10, -10:10)
 plot(vb_result$lb, ylab="ELBO", xlab="Iteration", main="ELBO plot")
+
+
+# Semiparametric regression with Measurement Error --------------------
+
+source("semimer.R")
+set.seed(10)
+N = 130
+D = 6
+RR = 0.9
+xi2 = 0.8
+sig2v = xi2/RR-xi2
+mux = 1.5
+beta = rnorm(D+1)
+w = matrix(rnorm(N*D),N,D)
+x = rnorm(N, mux, sqrt(xi2))
+v = rnorm(N, x, sqrt(sig2v))
+f = function(x) 2*x+2*sin(pi*x)
+y = cbind(1,w)%*%beta + f(x) + rnorm(N, sd=0.7)
+vb_result = semimer(y,w,v,n_intknot=5)
+plot(vb_result$lb, main="ELBO plot", ylab="ELBO", xlab="Iterations", sub=paste("Converged at",round(vb_result$lb[length(vb_result$lb)],3)))
+plot(beta[-1], vb_result$mubeta.q[-1], main="Fixed effects", xlab="True", ylab="Estimated")
+lines(-10:10,-10:10)
+plot(x,y-cbind(1,w)%*%vb_result$mubeta.q, main="Estimated mean curve", ylab="Residual")
+ord = order(vb_result$xgrid)
+lines(vb_result$xgrid[ord],vb_result$post_curve[ord],lwd=3,col=2)
+lines(vb_result$xgrid[ord],vb_result$post_lower[ord],lwd=2,lty=2)
+lines(vb_result$xgrid[ord],vb_result$post_upper[ord],lwd=2,lty=2)
+
