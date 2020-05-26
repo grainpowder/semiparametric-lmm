25c73ddfdd1250fff0f287800a44ec678b51471b
Sunsik <siksicking@gmail.com>
2020-05-26 11:38:48 +0900

    Changed example to that of Wand(2008) appendix

--- MeasurementError/bspline/simulations.R
+++ MeasurementError/bspline/simulations.R
@@ -3,27 +3,25 @@
 # 1. Standard Non/Semiparametric ------------------------------------------
 
 source("regression.R")
+
+# Nonparametric
+# Example provided in Appendix of Wand(2008)
+library(lattice)
+x = environmental$radiation
+y = (environmental$ozone)^(1/3)
+vb_result = regression(y,x,n_intknot=2)
+plot(x,y,xlab="radiation", ylab="cuberoot of ozone", main="Result of B-spline basis")
+ord = order(x)
+lines(x[ord],vb_result$mubeta.q+vb_result$post_curve[ord],lwd=2,col=2)
+lines(x[ord],vb_result$mubeta.q+vb_result$post_upper[ord],lwd=2,lty=2)
+lines(x[ord],vb_result$mubeta.q+vb_result$post_lower[ord],lwd=2,lty=2)
+plot(vb_result$lb, ylab="ELBO", xlab="Iteration", main="ELBO plot")
+
+# Semiparametric
 set.seed(10)
 N = 100
 f = function(x) 2*x+sin(2*pi*x)
 x = runif(N) * 2
-
-# Nonparametric
-
-set.seed(10)
-y = 2 + f(x) + rnorm(N, sd=0.5)
-vb_result = regression(y,x)
-resid = y - vb_result$mubeta.q
-plot(x,resid,main="Nonparametric")
-ord = order(x)
-lines(x[ord],vb_result$post_curve[ord],lwd=2,col=2)
-lines(x[ord],vb_result$post_upper[ord],lwd=2,lty=2)
-lines(x[ord],vb_result$post_lower[ord],lwd=2,lty=2)
-plot(vb_result$lb, ylab="ELBO", xlab="Iteration", main="ELBO plot")
-
-# Semiparametric
-
-set.seed(10)
 D = 5
 w = matrix(rnorm(N*D), ncol=D)
 beta = rnorm(D+1)
